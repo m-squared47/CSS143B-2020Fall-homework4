f08a2480a32b3a3d026a794214851d7857c56042
Martin Matsudaira <mmats23@uw.edu>
2020-11-01 21:44:53 -0800

    binary search not finished

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -6,6 +6,42 @@ public class BinarySearch {
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
         // homework
-        return -1;  // place holder
+        int size = data.length;
+        int middle = size / 2;  //split the array in two, a low and a high
+        int middleOE = middle;
+        int[] low = null;              //low and high value arrays (assuming inputs are sorted)
+        int[] high = null;
+
+        if(size > 0){   //if the array has an element
+
+            if(size % 2 == 0) {                 //if array has an even number of elements
+                low = new int[middle];    //initialize
+                high = new int[middle];
+            }else{                              //if the array has an odd number of elements
+                middleOE = middle - 1;
+                low = new int[middle];
+                high = new int[middleOE];
+            }
+
+            for(int i = 0; i < middle; i++){            //fill in low array
+                low[i] = data[i];
+            }
+
+            for(int i = 0; i + middle < size; i++){    //fill in high array
+                high[i] = data[i + middle];
+            }
+
+            if(data[middle] == target){ //if by chance the middle value is the target, return the index
+                return middle - 1;
+            }
+
+            if(target < data[middle]){  //if the target is low, use recursion using the low array
+                return binarySearch(low, target);
+            }
+
+            return binarySearch(high, target);
+        }
+
+        return -1;  // if the target was not found or array is null
     }
 }
