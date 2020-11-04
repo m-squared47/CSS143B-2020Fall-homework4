e1be989f6a8e804959585b776654adbd15f9a625
Martin Matsudaira <mmats23@uw.edu>
2020-11-04 13:40:48 -0800

    binary search rewritten and finished

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -6,40 +6,21 @@ public class BinarySearch {
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
         // homework
-        int size = data.length;
-        int middle = size / 2;  //split the array in two, a low and a high
-        int middleOE = middle;
-        int[] low = null;              //low and high value arrays (assuming inputs are sorted)
-        int[] high = null;
+        int size = data.length;                 //size of data[]
+        int[] newData = null;
 
-        if(size > 0){   //if the array has an element
+        if(size > 0) {                          //if the array has more than 1 element
+            newData = new int[size - 1];        //data[] with size - 1
 
-            if(size % 2 == 0) {                 //if array has an even number of elements
-                low = new int[middle];    //initialize
-                high = new int[middle];
-            }else{                              //if the array has an odd number of elements
-                middleOE = middle - 1;
-                low = new int[middle];
-                high = new int[middleOE];
+            for (int i = 0; i < newData.length; i++) {    //fill in newData[] array
+                newData[i] = data[i];
             }
 
-            for(int i = 0; i < middle; i++){            //fill in low array
-                low[i] = data[i];
+            if (data[size - 1] != target) {    //if last element doesn't match
+                return binarySearch(newData, target);      //recursive call
+            }else{
+                return size - 1;                            //return index
             }
-
-            for(int i = 0; i + middle < size; i++){    //fill in high array
-                high[i] = data[i + middle];
-            }
-
-            if(data[middle] == target){ //if by chance the middle value is the target, return the index
-                return middle - 1;
-            }
-
-            if(target < data[middle]){  //if the target is low, use recursion using the low array
-                return binarySearch(low, target);
-            }
-
-            return binarySearch(high, target);
         }
 
         return -1;  // if the target was not found or array is null
