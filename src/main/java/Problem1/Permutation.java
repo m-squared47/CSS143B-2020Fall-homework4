94c64c88bba0f8981f34021b40a1e2f16e842363
Martin Matsudaira <mmats23@uw.edu>
2020-11-07 19:42:30 -0800

    permutations finished

--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -7,30 +7,56 @@ public class Permutation {
     public static List<List<Integer>> permute(int[] nums) {
         // homework
         //number of possible outcomes = n! (where n = number of elements)
-        if(nums == null){
-            return new ArrayList<List<Integer>>();
+
+        ArrayList<List<Integer>> perms = new ArrayList<List<Integer>>();    //list of permutations
+
+        if(nums == null || nums.length == 0){       //if nums is empty, return empty list
+            perms.add(toList(nums));                //add empty array into permutations list
+            return perms;                           //return empty arrayList
         }
 
-        ArrayList<Integer> numsList = new ArrayList<Integer>();
-        ArrayList<List<Integer>> permutations = new ArrayList<List<Integer>>();
+        boolean[] isUsed = new boolean[nums.length];                //which positions have been used
+        ArrayList<Integer> permuted = new ArrayList<Integer>();     //holds the permuted list
 
-        for(int num : nums){
-            numsList.add(num);
-        }
-
-        for(int i = 0; i < nums.length; i++) {
-            ArrayList<Integer> onePermutation = findPermutations(i, numsList);
-            permutations.add(onePermutation);
-        }
-        return permutations; // place holder
+        findPermutations(nums, permuted, isUsed, perms);            //finds all possible permutations
+                                                                    //(pass by reference)
+        return perms;   //return completed list
     }
 
-    private static ArrayList<Integer> findPermutations(int pos, ArrayList<Integer> inList){
-        ArrayList<Integer> onePerm = new ArrayList<Integer>();
+    private static void findPermutations(
+            int[] nums,                 //integer array
+            List<Integer> permuted,     //permuted list of nums
+            boolean[] isUsed,           //which positions have been used
+            List<List<Integer>> perms)  //all possible permutations
+    {
+        if(permuted.size() == nums.length){                 //if the list is filled
+            perms.add(new ArrayList<Integer>(permuted));    //add to list of possible permutations
+            return;                                         //exit
+        }
 
-        //if(inList.)
+        for(int i = 0; i < nums.length; i++){
+            if(isUsed[i]){  //if the number is being used, skip this iteration
+                continue;
+            }
 
+            isUsed[i] = true;                                   //mark this current index as being used
 
-        return onePerm;
+            permuted.add(nums[i]);                              //add current index into current permutation solution
+            findPermutations(nums, permuted, isUsed, perms);    //find permutations in all positions
+
+            permuted.remove(permuted.size() - 1);             //remove first element (move down)
+            isUsed[i] = false;                                  //reset used position to false
+        }
+    }
+
+    //changes array to arraylist
+    private static ArrayList<Integer> toList(int[] array){
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+
+        for(int element : array){
+            newList.add(element);
+        }
+
+        return newList;
     }
 }
